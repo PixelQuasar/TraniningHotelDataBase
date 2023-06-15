56c07d96dcd0d69d9ddf5edac88288b542426659
PixelQuasar <max281005@gmail.com>
2023-06-15 18:21:24 +0600

    post controller init

--- src/endPoints/hotelDB/postController.ts
+++ src/endPoints/hotelDB/postController.ts
@@ -5,18 +5,18 @@ const postController = express.Router()
 
 postController.post('/createPost', async (req: express.Request, res: express.Response) => {
     try {
-        const {content, authorId, images}
+        const {content, authorId, images} = req.body
 
         const data = {
-            content: string,
-            images: Array<string>,
-            views: number,
-            likes: number,
-            authorId: string,
-            active: boolean,
+            content: content,
+            images: images,
+            views: 0,
+            likes: 0,
+            authorId: authorId,
+            active: true,
         }
-        const newHotel = new hotelSchema(data)
-        const saveResponse = await newHotel.save()
+        const newPost = new postSchema(data)
+        const saveResponse = await newPost.save()
         res.send(saveResponse).status(200)
     }
     catch (error) {
@@ -24,5 +24,22 @@ postController.post('/createPost', async (req: express.Request, res: express.Res
         res.send(error).status(500)
     }
 })
+
+postController.post('/deleteByFilter', async (req: express.Request, res: express.Response) => {
+    try {
+        const filter = req.body.filter
+        if (filter) {
+            const mongoResponce = await postSchema.deleteMany(filter)
+            res.send(mongoResponce).status(200)
+            return
+        }
+        res.send("No filter").status(404)
+    } 
+    catch (error) {
+        console.log(error)
+        res.send(error).status(500)
+    }
+})
+
 
 export default postController
